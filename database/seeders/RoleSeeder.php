4fa2924b08b6b2cf069b4b3539ab48f278ec556e
Luis Gutierrez <lr.programming.tools@gmail.com>
2023-07-15 15:07:49 -0400

    Create roles with laravel spatie

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'regular user']);
+        $user = User::find(1);
+        $user->assignRole($role1);
+    }
+}
